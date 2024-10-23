bff22e89ac161621e40fc7518d6b02f4800a4e3b
junehanpark <jumehan2004@gmail.com>
2024-10-23 12:27:01 +0900

    Update 1-1 selection sorting.py

--- src/1-1 selection sorting.py
+++ src/1-1 selection sorting.py
@@ -1,10 +1,10 @@
-#selection sorting
+#selection top down sorting
 
 def selectionsort(list):
     for i in range(len(list)):
         min_idx=i
         for j in range(i+1,len(list)):
-            if list[min_idx]>list[j]:
+            if list[min_idx]<list[j]:
                 min_idx=j
         list[i],list[min_idx]=list[min_idx],list[i]
 
